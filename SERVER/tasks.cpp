debac462b77236cf675dc9db4a0c254af62e5f4c
ChungHuynhDiem <wowchdwow@gmail.com>
2024-02-16 14:01:29 +0300

    sever + function + database

---
+++ SERVER/tasks.cpp
@@ -0,0 +1,52 @@
+
+#include "tasks.h"
+#include <QRandomGenerator>
+#include <QString>
+#include <vector>
+#include <QVariantMap>
+
+int randomInt(int min, int max) {
+    return QRandomGenerator::global()->bounded(min, max + 1);
+}
+
+std::vector<std::vector<int>> generateMatrix3x3() {
+    std::vector<std::vector<int>> matrix(3, std::vector<int>(3));
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            matrix[i][j] = randomInt(-10, 10);
+        }
+    }
+
+    return matrix;
+}
+
+int determinant3x3(const std::vector<std::vector<int>>& matrix) {
+    return matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[2][1] * matrix[1][2])
+           - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+           + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+}
+
+QString matrixToString(const std::vector<std::vector<int>>& matrix) {
+    QString result;
+    for (const auto& row : matrix) {
+        for (int value : row) {
+            result += QString::number(value) + "\t";
+        }
+        result += "\n";
+    }
+    return result;
+}
+
+QVariantMap generateMatrixTask() {
+    QVariantMap task;
+
+    auto matrix = generateMatrix3x3();
+
+    int det = determinant3x3(matrix);
+
+    task["text"] = QString("Вычислите определитель следующей матрицы:\nМатрица A:\n%1").arg(matrixToString(matrix));
+    task["answer"] = QString("%1").arg(det);
+
+    return task;
+}
---
+++ SERVER/tasks.h
@@ -0,0 +1,23 @@
+/*#ifndef TASKS_H
+#define TASKS_H
+#include <QVariant>
+#include <QRandomGenerator>
+#include <QtMath>
+#include <vector>
+#include <QString>
+
+QVariantMap generateMatrixTask();
+
+#endif //TASKS_H*/
+
+#ifndef TASKS_H
+#define TASKS_H
+#include <QVariant>
+#include <QRandomGenerator>
+#include <QtMath>
+#include <vector>
+#include <QString>
+
+QVariantMap generateMatrixTask();
+
+#endif //TASKS_H
